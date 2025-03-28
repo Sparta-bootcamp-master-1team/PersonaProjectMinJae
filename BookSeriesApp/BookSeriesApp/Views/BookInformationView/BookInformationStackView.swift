3f4dfe26ec5adcb6babc91d97ecf91f470957caa
minjae <qlqlzbto@naver.com>
2025-03-28 17:40:20 +0900

    feat: BookInformationStackView Layout

--- BookSeriesApp/BookSeriesApp/Views/BookInformationView/BookInformationStackView.swift
+++ BookSeriesApp/BookSeriesApp/Views/BookInformationView/BookInformationStackView.swift
@@ -1,13 +1,45 @@
 import UIKit
+import SnapKit
 
-class BookInformationStackView: UIStackView {
+final class BookInformationStackView: UIStackView {
+    
+    private let bookImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleToFill
+        imageView.image = UIImage(named: "harrypotter1")
+        return imageView
+    }()
+    
+    private var bookAttributeStackView: UIStackView
     
     override init(frame: CGRect) {
+        bookAttributeStackView = BookAttributeStackView()
         super.init(frame: frame)
-        backgroundColor = .cyan
+        axis = .horizontal
+        distribution = .fill
+        alignment = .top
+        spacing = 10
+        layoutMargins = UIEdgeInsets(top: 0, left: 5, bottom: 0, right: 5)
+        isLayoutMarginsRelativeArrangement = true
+        addViews()
+        configureLayout()
     }
     
     required init(coder: NSCoder) {
+        bookAttributeStackView = BookAttributeStackView()
         super.init(coder: coder)
+    }
+    
+    private func addViews() {
+        addArrangedSubview(bookImageView)
+        addArrangedSubview(bookAttributeStackView)
+    }
+    
+    private func configureLayout() {
+        bookImageView.snp.makeConstraints { make in
+            make.top.equalToSuperview()
+            make.width.equalTo(100)
+            make.height.equalTo(150)
+        }
     }
 }
