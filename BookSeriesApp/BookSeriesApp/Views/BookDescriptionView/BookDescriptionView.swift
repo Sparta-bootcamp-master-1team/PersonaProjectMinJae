1039c593a42bdb8292c303fd16aa9bf344c79af6
minjae <qlqlzbto@naver.com>
2025-03-31 14:06:30 +0900

    feat: dir rename, init BookDescriptionView

---
+++ BookSeriesApp/BookSeriesApp/Views/BookDescriptionView/BookDescriptionView.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+class BookDescriptionView: UIStackView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
