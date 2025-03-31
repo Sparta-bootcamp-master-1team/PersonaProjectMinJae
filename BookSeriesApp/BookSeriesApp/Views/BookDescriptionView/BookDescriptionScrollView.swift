84e2324cbb198eeb5926b802523393059dfde648
minjae <qlqlzbto@naver.com>
2025-03-31 17:07:18 +0900

    feat: Set ChapterStackView UI property layout and data bind

--- BookSeriesApp/BookSeriesApp/Views/BookDescriptionView/BookDescriptionScrollView.swift
+++ BookSeriesApp/BookSeriesApp/Views/BookDescriptionView/BookDescriptionScrollView.swift
@@ -4,10 +4,12 @@ class BookDescriptionScrollView: UIScrollView {
     
     private let bookDescriptionStackView: BookDescriptionStackView
     private var bookInformationStackView: BookInformationStackView
+    private let bookChaptersStackView: BookChaptersStackView
     
     override init(frame: CGRect) {
         self.bookDescriptionStackView = BookDescriptionStackView()
         self.bookInformationStackView = BookInformationStackView()
+        self.bookChaptersStackView = BookChaptersStackView()
         super.init(frame: frame)
         backgroundColor = .cyan
         addViews()
@@ -17,12 +19,14 @@ class BookDescriptionScrollView: UIScrollView {
     required init?(coder: NSCoder) {
         self.bookInformationStackView = BookInformationStackView()
         self.bookDescriptionStackView = BookDescriptionStackView()
+        self.bookChaptersStackView = BookChaptersStackView()
         super.init(coder: coder)
     }
     
     private func addViews() {
         self.addSubview(bookDescriptionStackView)
         self.addSubview(bookInformationStackView)
+        self.addSubview(bookChaptersStackView)
     }
     
     private func configureLayout() {
@@ -35,9 +39,15 @@ class BookDescriptionScrollView: UIScrollView {
             $0.top.equalTo(self.bookInformationStackView.snp.bottom)
             $0.leading.trailing.equalToSuperview()
         }
+        bookChaptersStackView.snp.makeConstraints { [weak self] in
+            guard let self else { return }
+            $0.top.equalTo(self.bookDescriptionStackView.snp.bottom)
+            $0.leading.trailing.equalToSuperview()
+        }
     }
     
     func bind(model: BookAttribute, index: Int) {
         bookInformationStackView.bind(model: model, index: index)
+        bookChaptersStackView.bind(model: model)
     }
 }
