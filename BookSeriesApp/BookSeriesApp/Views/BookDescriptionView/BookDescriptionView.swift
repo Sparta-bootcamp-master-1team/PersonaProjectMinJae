e3615feff576598d63c982036c62a8c6b24c3d35
minjae <qlqlzbto@naver.com>
2025-03-31 14:13:24 +0900

    feat: UI property

--- BookSeriesApp/BookSeriesApp/Views/BookDescriptionView/BookDescriptionView.swift
+++ BookSeriesApp/BookSeriesApp/Views/BookDescriptionView/BookDescriptionView.swift
@@ -1,6 +1,37 @@
 import UIKit
 
 class BookDescriptionView: UIStackView {
+    
+    private let dedicationLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        label.font = .systemFont(ofSize: 18, weight: .bold)
+        return label
+    }()
+    
+    private let dedicationTextLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        label.font = .systemFont(ofSize: 14, weight: .regular)
+        label.textColor = .darkGray
+        return label
+    }()
+    
+    private let summaryLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        label.font = .systemFont(ofSize: 18, weight: .bold)
+        return label
+    }()
+    
+    private let summaryTextLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        label.font = .systemFont(ofSize: 14, weight: .regular)
+        label.textColor = .darkGray
+        return label
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
     }
