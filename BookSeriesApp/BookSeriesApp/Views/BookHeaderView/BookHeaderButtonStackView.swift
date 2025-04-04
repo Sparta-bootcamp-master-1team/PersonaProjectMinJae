0dc178fa4c9306a222e7981791dd7872327a4181
minjae <qlqlzbto@naver.com>
2025-04-04 10:34:33 +0900

    docs: BookHeaderButtonStackView comment

--- BookSeriesApp/BookSeriesApp/Views/BookHeaderView/BookHeaderButtonStackView.swift
+++ BookSeriesApp/BookSeriesApp/Views/BookHeaderView/BookHeaderButtonStackView.swift
@@ -7,8 +7,11 @@ protocol BookHeaderButtonStackViewDelegate: AnyObject {
 final class BookHeaderButtonStackView: UIStackView {
     
     private var buttons: [UIButton] = []
+    // 가장 최근에 선택된 시리즈 인덱스
     private var lastButtonIndex: Int = 0
+    // 현재 선택된 시리즈 인덱스
     private var currentButtonSelectedIndex: Int = 0
+    // 데이터로 받아온 시리즈의 총 개수
     private var totalCount: Int = 0 {
         didSet {
             if totalCount == oldValue { return }
@@ -38,6 +41,7 @@ final class BookHeaderButtonStackView: UIStackView {
         self.alignment = .top
     }
     
+    // 시리즈 개수만큼 버튼 생성
     private func addArrangedSubviews(count: Int) {
         for i in 1...count {
             let button: UIButton = {
@@ -63,12 +67,14 @@ final class BookHeaderButtonStackView: UIStackView {
         }
     }
     
+    // 버튼 클릭 액션
     @objc func buttonTapped(_ sender: UIButton) {
         guard let buttonIndex = Int(sender.titleLabel?.text ?? "0") else { return }
         delegate?.buttonTapped(at: buttonIndex-1)
         setButtonState(at: buttonIndex-1)
     }
     
+    // 버튼 클릭시 클릭된 버튼 하이라이트, 이전 버튼은 다시 원래 상태로 전환
     private func setButtonState(at current: Int) {
         if lastButtonIndex == current { return }
         
